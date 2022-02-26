a260e662a0720aea97b189db224e01451f513955
Phil Bailey <philbailey@msn.com>
2022-02-25 19:35:26 -0500

    add update schema, readme for date custom validation

--- models/bug.js
+++ models/bug.js
@@ -7,7 +7,17 @@ const bugSchema = new mongoose.Schema({
 	},
 	info: {
 		submitter_name: String,
-		date_reported: Date, //mm/dd/yyyy
+		date_reported: {
+			type: String,
+			validate: {
+				validator: function (value) {
+					return /(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/.test(
+						value
+					);
+				},
+				message: props => ` ${props.value} Date must be mm/dd/yyyy`,
+			},
+		},
 		open: Boolean, //Bug report open or closed
 	},
 	where: {
